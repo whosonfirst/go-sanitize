7d5897d39a7501a8a371f6a6b4c40c0f2a8c70f3
thisisaaroland <aaron@mapzen.com>
2016-09-09 12:49:05 -0700

    comments

--- cmd/test.go
+++ cmd/test.go
@@ -6,45 +6,51 @@ import (
 	"fmt"
 	"os"
 	"sanitize"
-	_ "unicode"
 	"strconv"
+	_ "unicode"
 )
 
 func main() {
 
 	opts := sanitize.DebugOptions()
 
+	// see below...
+	
 	codepoints := [][]string{
-	       { "00000000", "00001000" },
-	       { "00001110", "00011111" },
-	       { "01111111", "10000100" },
-	       { "10000110", "10011111" },
-	       { "1111111011111111", "1111111011111111" },
-	       { "1111111111111001", "1111111111111010" },
-	       { "11100000000000000000", "11100000000001111111" },
-	       { "1101100000000000", "1101111111111111" },
-	       { "100010000000000000000", "100111111111111111111" },
+		{"00000000", "00001000"},
+		{"00001110", "00011111"},
+		{"01111111", "10000100"},
+		{"10000110", "10011111"},
+		{"1111111011111111", "1111111011111111"},
+		{"1111111111111001", "1111111111111010"},
+		{"11100000000000000000", "11100000000001111111"},
+		{"1101100000000000", "1101111111111111"},
+		{"100010000000000000000", "100111111111111111111"},
 	}
 
 	for _, pair := range codepoints {
-	
-	lo, _ := strconv.ParseUint(pair[0], 2, 32)
-	hi, _ := strconv.ParseUint(pair[1], 2, 32)	
 
-	// fmt.Println("#", lo, hi)
-	
-	for i := lo; i < hi; i++ {
-	
-		r := rune(i)
-		c, _ := sanitize.SanitizeString(string(r), opts)
+		// I studied painting... leave me alone or just be
+		// gentle when you show me how to do this the right
+		// way... (20160909/thisisaaronland)
+		
+		lo, _ := strconv.ParseUint(pair[0], 2, 32)
+		hi, _ := strconv.ParseUint(pair[1], 2, 32)
 
-		if c != " { SANITIZED } " {
-		   fmt.Printf("%b %U '%s'\n", r, r, c)
+		// fmt.Println("#", lo, hi)
+
+		for i := lo; i < hi; i++ {
+
+			r := rune(i)
+			c, _ := sanitize.SanitizeString(string(r), opts)
+
+			if c != " { SANITIZED } " {
+				fmt.Printf("%b %U '%s'\n", r, r, c)
+			}
 		}
 	}
-	}
 	os.Exit(0)
-	
+
 	input := "foo bar\nbaz ok: '\u2318' BAD:'\u0007' BAD:'\uFEFF' BAD:'\u2029' BAD:'\u0007' BAD:'\u007F' BAD:'\x0B' woop wwoop"
 
 	for index, runeValue := range input {
